1090d7fb90d436c0f4872b133b1e476752812c9f
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-06-07 21:52:20 +0300

    ex 2

--- LAB5/Program.cs
+++ LAB5/Program.cs
@@ -6,9 +6,20 @@ namespace LAB5
     {
         static void Main(string[] args)
         {
+            //Ex1();
+            Ex2();
+            //Ex3();
+            //Ex4();
+            //Ex5();
+            //Ex6();
+            //Ex7();
+        }
+        static void Ex1()
+        //sa se scrie un prg care va afisa pozitia unui substring intr-un string, ambele siruri citite de la tast.
+        {
             string str1;
             string str2;
-            
+
             Console.WriteLine("Introduceti string1 ");
             str1 = Console.ReadLine();
 
@@ -18,12 +29,36 @@ namespace LAB5
             if (index < 0)
                 Console.WriteLine("Nu exista string2 ");
             else
-                Console.WriteLine("Found '{0}' in '{1}' at position {2}", str2, str1, index+1);
+                Console.WriteLine("Found '{0}' in '{1}' at position {2}", str2, str1, index);
 
         }
-
+        static void Ex2()
+        //scrieti un program care sa verifice daca un sir de caractere citit de la tastatura este sau nu palindrom
+        {
+            
+           
+            Console.Write("Introduceti un string : ");
+            string inputstr = Console.ReadLine();
+            string reversestr = string.Empty;
+            if (inputstr != null)
+            {
+                for (int i = inputstr.Length - 1; i >= 0; i--)
+                {
+                    reversestr += inputstr[i].ToString();
+                }
+                if (reversestr == inputstr)
+                {
+                    Console.WriteLine("String este Palindrom Initial= {0} and Final= {1}", inputstr, reversestr);
+                }
+                else
+                {
+                    Console.WriteLine("String nu este Palindrom Input = {0} and Output= {1}", inputstr, reversestr);
+                }
+            }
+            Console.ReadLine();
         }
     }
 
+}
 
 
